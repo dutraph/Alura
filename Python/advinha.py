cd1f8cdd4f4bc12dfb58965af13778142df893c7
Paulo Dutra <dutra.phs@gmail.com>
2020-06-11 19:10:48 -0300

    add

--- Python/advinha.py
+++ Python/advinha.py
@@ -1,17 +1,17 @@
 print("**********************")
-print("Bem vindo ao adivinha!")
+print("Bem vindo ao Cara ou Coroa!")
 print("**********************")
 
 nome = input("Qual seu nome: ")
 def jg():
     from random import randint
     sec_num = (randint(0,1))
-    chute = int(input("Digite um numero de 0 a 1: "))
+    chute = int(input("Digite (0 para Cara) e (1 para Coroa): "))
 
-    if(sec_num == chute):
-        print("Parabens %s, voce acertou" %nome, end="\n\n")
+    if(sec_num == chute and chute == 0):
+        print("Cara", end="\n\n")
     else:
-        print("Voce errou!!!", end="\n\n")
+        print("Coroa", end="\n\n")
 
     replay = str(input("Deseja jogar novamente?[s/n]: "))
 
