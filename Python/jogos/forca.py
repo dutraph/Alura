b856e769e2418a243ba5e57743a5c9fc73be5683
Paulo MackBook Pro <dutra.phs@gmail.com>
2020-06-14 17:30:34 -0300

    add

--- Python/jogos/forca.py
+++ Python/jogos/forca.py
@@ -1,26 +1,31 @@
 def jg_forca():
+    import getpass
     print("***********************")
     print("  Bem vindo ao Forca!  ")
     print("***********************", end="\n\n")
 
-    palavra_secreta,tipo = input('Qual a palavra e o tipo: ').split()
-    print(f'Dica: a palavra secreta tem {len(palavra_secreta)} letras, e eh um(a) {tipo}. Boa sorte.')
-    
+    #palavra_secreta,tipo = input('Qual a palavra e o tipo: ').split()
+    #print(f'Dica: a palavra secreta tem {len(palavra_secreta)} letras, e eh um(a) {tipo}. Boa sorte.')
+    palavra_secreta = getpass.getpass('Digite a palavra secreta: ')
+    size = len(palavra_secreta)
+    print(f'Dica: a palavra tem {size} letras')
+    letras_acertadas = ['_', '_', '_', '_']
+    #letras_acertadas.append(size * ('_',))
     enforcou = False
     acertou = False
     # enquanto True e True 
     while not enforcou and not acertou:
-        lista = []
+        
         chute = input('Qual letra?: ')
         chute = chute.strip()
 
-        lista = []
         index = 0
         for letra in palavra_secreta:
             if chute.upper() == letra.upper():
+                letras_acertadas[index] = letra
                 print(f'Encontrei a letra "{letra}" na posicao {index}.')
             index = index + 1
-        print('Jogando...')
+        print(letras_acertadas)
 
     print('Fim do Jogo')
 
--- Python/jogos/palavra.py
+++ Python/jogos/palavra.py
@@ -3,13 +3,14 @@ def palavra_secreta():
     lista_fruta = ['banana', 'uva', 'abacate', 'melao']
     lista_eletronico = ['impressora', 'celular', 'tablet', 'computador']
 
-    tipo = input('Escolha o tipo de palavra, frutas ou eletronicos: ')
-    if tipo == 'frutas':
+    tipo = input('Escolha o tipo de palavra, (1)frutas  ou (2)eletronicos: ')
+    if tipo == '1':
         escolha_random = random.choice(lista_fruta)
-    elif tipo == 'eletronicos':
+    elif tipo == '2':
         escolha_random = random.choice(lista_eletronico)
 
     print(escolha_random)
 
 if(__name__ == '__main__'):
     palavra_secreta()
+
