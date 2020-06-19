050932276122b6d2b1b88ac3732bb07930cd9001
Paulo MackBook Pro <dutra.phs@gmail.com>
2020-06-18 22:21:22 -0300

    add

--- Python/python_Orie_Objet/conta.py
+++ Python/python_Orie_Objet/conta.py
@@ -15,9 +15,18 @@ class ContaCorrente:
         self.__saldo += valor
         print(f'({self.__titular}): Depositando {valor}, seu saldo atual é {self.__saldo} ')
     
+
+    def __pode_sacar(self, valor_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_sacar <= valor_disponivel
+
     def saque(self, valor):
-        self.__saldo -= valor
-        print(f'({self.__titular}): Saque de R${valor} realizado, seu saldo atual é {self.__saldo}.')
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+            print(f'({self.__titular}): Saque de R${valor} realizado, seu saldo atual é {self.__saldo}.')
+        else:
+            print(f'Operaçã invalida! o valor de {valor} ultrapassa limite permitido.')
+        
 
     def transfere(self, valor, destino):
         valor = valor
@@ -27,17 +36,28 @@ class ContaCorrente:
         print()
         self.extrato()
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
-    
-    def get_titular(self):
-        return self.__titular
+
+    @property
+    def titular(self):
+        return self.__titular.title()
+    @titular.setter
+    def titular(self, nome):
+        self.__titular = nome
 
     @property
     def limite(self):
         return self.__limite
-
     @limite.setter
     def limite(self, limite):
         self.__limite = limite
-        
+
+    @staticmethod
+    def codigo_banco():
+        return '001'
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
