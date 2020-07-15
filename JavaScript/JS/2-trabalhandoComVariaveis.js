3904586303b6132f3b5c87c7cf45b586fac28e71
Paulo MackBook Pro <dutra.phs@gmail.com>
2020-07-15 18:10:49 -0300

    add

---
+++ JavaScript/JS/2-trabalhandoComVariaveis.js
@@ -0,0 +1,15 @@
+console.log('Trab com variaveis');
+
+var idadeAluno = 23;
+var k = 1;
+while (k >= idadeAluno){
+  k++;
+  console.log(k);
+}
+
+/*
+if (idadeAluno > 20){
+    console.log('Nao tem idade para ser aluno')
+} else {
+    console.log('Idade permitida!')
+}*/
---
+++ JavaScript/JS/3-operaçoesAritimeticas.js
@@ -0,0 +1,3 @@
+console.log('Operaçoes aritimeticas');
+
+console.log(10 + 8 * 2);
